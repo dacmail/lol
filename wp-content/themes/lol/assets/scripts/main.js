01bb1992e67745db47466ef66716e75cfed41188
Daniel Aguilar <dacmail@gmail.com>
2017-10-17 11:54:01 +0200

    Scroll menu

--- wp-content/themes/lol/assets/scripts/main.js
+++ wp-content/themes/lol/assets/scripts/main.js
@@ -19,6 +19,17 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
+        $('a[href*="#"]').on('click', function() {
+          if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+              var targetOffset = $target.offset().top;
+              $('html,body').animate({scrollTop: targetOffset }, 1000);
+              return false;
+            }
+          }
+        });
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
